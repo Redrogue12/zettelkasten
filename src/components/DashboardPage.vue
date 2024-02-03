571099e77a85ef053c216860ec312fb351d6562c
Redrogue12 <exgc12@gmail.com>
2024-02-02 19:12:54 -0700

    feat: pull notes

--- src/components/DashboardPage.vue
+++ src/components/DashboardPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="dashboard">
-    <div class="note-card" v-for="note in notes" :key="note.id">
-      <h2>{{ note.title }}</h2>
-      <p>{{ note.content }}</p>
+    <div
+      class="note-card"
+      v-for="note in notes"
+      :key="note.id"
+    >
+      <h2>{{ note.note_title }}</h2>
+      <p>{{ note.note_text }}</p>
     </div>
   </div>
 </template>
@@ -12,22 +16,16 @@ export default {
   data() {
     return {
       name: 'Dashboard Page',
-      notes: [
-        { id: 1, title: 'Note 1', content: 'This is note 1' },
-        { id: 2, title: 'Note 2', content: 'This is note 2' },
-        { id: 3, title: 'Note 3', content: 'This is note 3' },
-        { id: 4, title: 'Note 4', content: 'This is note 4' },
-        { id: 5, title: 'Note 5', content: 'This is note 5' },
-        { id: 6, title: 'Note 6', content: 'This is note 6' },
-        { id: 1, title: 'Note 1', content: 'This is note 1' },
-        { id: 2, title: 'Note 2', content: 'This is note 2' },
-        { id: 3, title: 'Note 3', content: 'This is note 3' },
-        { id: 4, title: 'Note 4', content: 'This is note 4' },
-        { id: 5, title: 'Note 5', content: 'This is note 5' },
-        { id: 6, title: 'Note 6', content: 'This is note 6' },
-        // Add more notes here...
-      ],
+      notes: [],
     };
+  },
+  async created() {
+    try {
+      const response = await fetch('http://localhost:3000/notes');
+      this.notes = await response.json();
+    } catch (error) {
+      console.error('Failed to fetch notes');
+    }
   },
 };
 </script>
