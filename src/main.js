9e3cd89bd141f9c439da44005a441d7f06691e40
Redrogue12 <exgc12@gmail.com>
2024-02-17 20:58:59 -0700

    chore: add fontawesome

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faEdit);
+library.add(faEdit, faLink);
 
 const app = createApp(App);
 app.component("font-awesome-icon", FontAwesomeIcon);
