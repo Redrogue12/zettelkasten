524ddeadc5b4571cf67262f05e6b8d3974da4cb5
Redrogue12 <exgc12@gmail.com>
2024-03-09 14:40:15 -0700

    NoteCard tests done

--- src/components/Notes/tests/NoteCard.test.js
+++ src/components/Notes/tests/NoteCard.test.js
@@ -4,15 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import NoteCard from "@/components/Notes/NoteCard.vue";
 
 describe("NoteCard", () => {
-  it("renders note title and text correctly", () => {
-    const note = {
-      id: 1,
+  let wrapper;
+  let note;
+  beforeEach(() => {
+    note = {
+      note_id: 1,
       note_title: "Test Note",
       note_text: "This is a test note",
       date_created: "2021-01-01",
       date_modified: "2021-01-01",
     };
-    const wrapper = shallowMount(NoteCard, {
+    wrapper = shallowMount(NoteCard, {
       propsData: {
         note,
       },
@@ -22,30 +24,13 @@ describe("NoteCard", () => {
         },
       },
     });
-
+  });
+  it("renders note title and text correctly", () => {
     expect(wrapper.find("h5").text()).toBe(note.note_title);
     expect(wrapper.find("p").text()).toBe(note.note_text);
   });
 
   it("when note is clicked, emit click event and pass correct value", () => {
-    const note = {
-      id: 1,
-      note_title: "Test Note",
-      note_text: "This is a test note",
-      date_created: "2021-01-01",
-      date_modified: "2021-01-01",
-    };
-    const wrapper = shallowMount(NoteCard, {
-      propsData: {
-        note,
-      },
-      global: {
-        components: {
-          FontAwesomeIcon,
-        },
-      },
-    });
-
     wrapper.trigger("click");
 
     expect(wrapper.emitted("click")).toBeTruthy();
