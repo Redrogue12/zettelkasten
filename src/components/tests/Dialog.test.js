afde38ffa70e12194c72ab15f5f1b97023461f08
Redrogue12 <exgc12@gmail.com>
2024-03-13 13:57:56 -0600

    test: refactor dialog.test.js

--- src/components/tests/Dialog.test.js
+++ src/components/tests/Dialog.test.js
@@ -8,8 +8,9 @@ import Dialog from "@/components/Dialog.vue";
 library.add(faTimes);
 
 describe("Dialog", () => {
-  it("renders dialog", () => {
-    const wrapper = shallowMount(Dialog, {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(Dialog, {
       propsData: {
         open: true,
       },
@@ -19,42 +20,20 @@ describe("Dialog", () => {
         },
       },
     });
-
+  });
+  it("renders dialog", () => {
     expect(wrapper.find("div.dialog")).toBeTruthy();
   });
 
   it("when clicking close button emits 'close-dialog'", () => {
-    const wrapper = shallowMount(Dialog, {
-      propsData: {
-        open: true,
-      },
-      global: {
-        components: {
-          FontAwesomeIcon,
-        },
-      },
-    });
     wrapper.find(".close-icon").trigger("click");
-
     expect(wrapper.emitted("close-dialog")).toBeTruthy();
   });
 
   it("closes the dialog when a click is done outside", async () => {
-    const wrapper = shallowMount(Dialog, {
-      propsData: {
-        open: true,
-      },
-      global: {
-        components: {
-          FontAwesomeIcon,
-        },
-      },
-    });
     const closeDialog = jest.spyOn(wrapper.vm, "closeDialog");
-
     await wrapper.vm.$nextTick();
     document.body.click();
-
     expect(closeDialog).toHaveBeenCalled();
   });
 });
